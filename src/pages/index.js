286f870f820c88812f13f0e95605987c57392563
brittanyrw <brittany.walker@socialdriver.com>
2019-03-11 17:21:32 -0400

    update component structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,24 @@
 import React from "react"
 import Nav from "../components/nav"
+import Header from "../components/header"
+import Project from "../components/project"
+import Teaching from "../components/teaching"
+import About from "../components/about"
 import Footer from "../components/footer"
 
 export default () => (
 	<div>
 		<Nav/>
-		<div>Hello world!</div>
+		<Header/>
+		<main>
+			<Project/>
+			<Project/>
+			<Project/>
+			<Project/>
+			<Project/>
+			<Teaching/>
+			<About/>
+		</main>
 		<Footer/>
 	</div>
 )
